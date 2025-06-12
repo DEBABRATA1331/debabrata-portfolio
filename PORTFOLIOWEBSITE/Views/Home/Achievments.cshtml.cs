ffc73448a090d836f1a7a294fda68f9ecf226748
sanjib.sahoo <11022492@vedanta.co.in>
2025-06-12 13:35:48 +0530

    Initial code

---
+++ PORTFOLIOWEBSITE/Views/Home/Achievments.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PORTFOLIOWEBSITE.Views.Home
+{
+    public class AchievmentsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
