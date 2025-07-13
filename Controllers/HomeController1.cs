736d64e1c71cbb734baa1d0f26b91468e11df71f
= <=>
2025-07-13 22:01:22 +0530

    Moved project files to root for deployment

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PORTFOLIOWEBSITE.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult About() => View();
+        public IActionResult Projects() => View();
+        
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PORTFOLIOWEBSITE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
